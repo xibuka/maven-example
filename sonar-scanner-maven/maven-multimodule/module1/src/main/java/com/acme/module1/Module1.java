54b675553ab34f82ad224bdce463218f96348f61
Wenhan Shi <wenhan.shi@sonarsource.com>
2025-09-08 00:15:50 +0900

    Update Module1.java

--- sonar-scanner-maven/maven-multimodule/module1/src/main/java/com/acme/module1/Module1.java
+++ sonar-scanner-maven/maven-multimodule/module1/src/main/java/com/acme/module1/Module1.java
@@ -3,7 +3,7 @@ package com.acme.module1;
 public class Module1 {
 
   public void coveredByUnitTest() {
-    System.out.println("This method is covered by unit test");
+    System.out.println("This method is covered by unit test2");
   }
 
   public void coveredByIntegrationTest() {
